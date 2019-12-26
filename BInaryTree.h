40be4af87f0e37d84b34ae1b011afad96608dd59
Student <buddhagautam231@gmail.com>
2019-12-26 10:01:17 +0545

    hello

--- BInaryTree.h
+++ BInaryTree.h
@@ -1,6 +1,10 @@
-#include<iostream>
-class BinaryTree{
+#ifdef BinaryTree_h
+#define BinaryTree_h
+ BinarySearchTree{
 	private:
 	public: 
-		
+		virtual void preorderTransersal()=0;
+		virtual void add(int data)=0;
+		virtual bool search(int data)=0;
 };
+#endif
